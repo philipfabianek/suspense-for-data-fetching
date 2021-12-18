a981879cdf82a888f597c8bc4d5a7ef4ca51a415
Philip <fabianekphilip@gmail.com>
2021-12-18 08:01:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react';
+import { fetch } from 'react-fetch';
+
+function UserSkeleton() {
+  return (
+    <div className="user-skeleton" />
+  );
+};
+
+function User() {
+  const endpoint = "https://jsonplaceholder.typicode.com/users/1";
+  const user = fetch(endpoint).json();
+
+  return (
+    <p className="user-text">
+      {user.name}{" "}<b>@{user.username}</b>
+    </p>
+  );  
+};
+
+function PostSkeleton() {
+  return (
+    <div className="skeleton">
+      <div className="skeleton__title" />
+      <div className="skeleton__text" />
+      <div className="skeleton__text" />
+      <div className="skeleton__text" />
+    </div>
+  );
+}
+
+function Posts() {
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  const posts = fetch(endpoint).json();
+
+  return (
+    <div>
+      {
+        posts.map(post => (
+          <div className='post' key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))
+      }
+    </div>
+  );  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={<UserSkeleton />}>
+        <User />
+      </Suspense>
+      <div className='container'>
+        <Suspense fallback={(<div>
+          <PostSkeleton /><PostSkeleton />
+          <PostSkeleton /><PostSkeleton />
+          <PostSkeleton /><PostSkeleton />
+          <PostSkeleton /><PostSkeleton />
+          <PostSkeleton /><PostSkeleton />
+        </div>)}>
+          <Posts />
+        </Suspense>
+      </div>
     </div>
   );
 }
